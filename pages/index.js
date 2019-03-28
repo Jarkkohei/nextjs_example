3bd01eca69b92ffae70a063f581dc171640ca54f
Jarkko <jarkkohei@luukku.com>
2019-03-28 13:10:04 +0200

    Add getInitialProps-lifecyclehook to pre-populate the props

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,20 @@ import Router from 'next/router';
 
 class IndexPage extends Component {
 
+    static async getInitialProps(context) {
+        console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({appName: 'nextjs_example'});
+            }, 1000);
+        });
+        return promise;
+    }
+
     render() {
         return (
             <div>
-                <h1>The Main page</h1>
+                <h1>The Main page of {this.props.appName}</h1>
 
                 {/* Navigate using a Link */}
                 <Link href="/auth"><a>Auth page</a></Link>
