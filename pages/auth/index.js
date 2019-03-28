86ad9076ef6d25ffdb2d4435b906287f64b0b079
Jarkko <jarkkohei@luukku.com>
2019-03-28 13:17:31 +0200

    Pass props to authIndexPage and add an example of getInitialProps-lifecyclehook usage in functional component

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -4,9 +4,9 @@ import Router from 'next/router';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>Auth Page</h1>
+        <h1>Auth Page - {props.appName}</h1>
 
         {/* Navigate using a Link */}
         <Link href="/"><a>Main page</a></Link>
@@ -16,5 +16,14 @@ const authIndexPage = () => (
         <User name="Ronald" age={25} />
     </div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: 'nextjs_example (Auth)' });
+        }, 1000);
+    });
+    return promise;
+}
 
 export default authIndexPage;
