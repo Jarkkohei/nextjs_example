392869053056fb78e6c3677229387d8e0e85e55d
Jarkko <jarkkohei@luukku.com>
2019-03-28 12:22:10 +0200

    Add links to move between Main- and Auth-pages with anchor-elements

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from  'react';
+import Link from 'next/link';
 
 const authIndexPage = () => (
     <div>
         <h1>Auth Page</h1>
+        <Link href="/"><a>Main page</a></Link>
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Link from 'next/link';
 
 const indexPage = () => (
     <div>
         <h1>The Main page</h1>
+        <Link href="/auth"><a>Auth page</a></Link>
     </div>
 );
 
