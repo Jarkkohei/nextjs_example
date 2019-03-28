00bb9b7fd11d8ab05f4689862d1b3b33ddb14aec
Jarkko <jarkkohei@luukku.com>
2019-03-28 12:26:38 +0200

    Import Router and add navigation via it with button

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,16 @@
 import React from  'react';
 import Link from 'next/link';
+import Router from 'next/router'
 
 const authIndexPage = () => (
     <div>
         <h1>Auth Page</h1>
+
+        {/* Navigate using a Link */}
         <Link href="/"><a>Main page</a></Link>
+
+        {/* Navigate using the Router */}
+        <p><button onClick={() => Router.push('/')}>Main</button></p>
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Router from 'next/router';
 
 const indexPage = () => (
     <div>
         <h1>The Main page</h1>
+
+        {/* Navigate using a Link */}
         <Link href="/auth"><a>Auth page</a></Link>
+
+        {/* Navigate using the Router */}
+        <p><button onClick={() => Router.push('/auth')}>Auth</button></p>
     </div>
 );
 
