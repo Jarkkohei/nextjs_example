410289d89306bbcf7166b9e13458d334dac18b22
Jarkko <jarkkohei@luukku.com>
2019-03-28 13:02:10 +0200

    Refactor the Main-page into a Class-based component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>The Main page</h1>
+class IndexPage extends Component {
 
-        {/* Navigate using a Link */}
-        <Link href="/auth"><a>Auth page</a></Link>
+    render() {
+        return (
+            <div>
+                <h1>The Main page</h1>
 
-        {/* Navigate using the Router */}
-        <p><button onClick={() => Router.push('/auth')}>Auth</button></p>
-    </div>
-);
+                {/* Navigate using a Link */}
+                <Link href="/auth"><a>Auth page</a></Link>
 
-export default indexPage;
+                {/* Navigate using the Router */}
+                <p><button onClick={() => Router.push('/auth')}>Auth</button></p>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
